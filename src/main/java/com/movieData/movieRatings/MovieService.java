a176fc6fd87325c910056de899310e6e0e341dee
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2025-12-27 23:33:03 +0100

    new functions added to the movieService to look up and add movies to the database

--- src/main/java/com/movieData/movieRatings/MovieService.java
+++ src/main/java/com/movieData/movieRatings/MovieService.java
@@ -1,6 +1,7 @@
 package com.movieData.movieRatings;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +35,44 @@ public class MovieService {
         return movieRepository.findAll().stream()
             .filter(movie -> movie_uri.equals(movie.getMovie_URI()))
             .collect(Collectors.toList());
+    }
+    
+    //Grabs the movie name
+    public List<Movies> getMovieByName(String searchText) {
+        return movieRepository.findAll().stream().filter(movie -> movie.getMovie_name().toLowerCase().contains(searchText.toLowerCase())).collect(Collectors.toList());
+    }
+
+
+    //Grabs when the movie was made
+
+    public List<Movies> getMovieYear(Integer movieYear) {
+        return movieRepository.findAll().stream()
+        .filter(movie -> movieYear.equals(movie.getMovie_year()))
+        .collect(Collectors.toList());
+    }
+
+    //Grabs a movie that matches the given year and the given movie name
+    public List<Movies> getMovieByYearandName(Integer movieYear, String searchText) {
+        return movieRepository.findAll().stream()
+        .filter(movies -> movieYear.equals(movies.getMovie_year()) && searchText.equals(movies.getMovie_name()))
+        .collect(Collectors.toList());
+    }
+
+    //adds a new movie
+    public Movies addMovie(Movies movie) {
+        movieRepository.save(movie);
+        return movie; // returns the movie so we can see what we saved
+    }
+
+    //updates an existing movie
+    public Movies updateMovie(Movies updateMovie) {
+        Optional<Movies> existingMovies = movieRepository.findByName(updateMovie.getMovie_name());
+
+        if(existingMovies.isPresent()) {
+            Movies movieToUpdate = existingMovies.get();
+            movieToUpdate.setMovie_name(updateMovie.getMovie_name());
+        }
+
+        
     }
 }
